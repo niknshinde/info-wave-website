4407446e63486d41b1c9d977ad406a0657663de1
niknshinde <ns8830603789@gmail.com>
2023-08-04 14:15:47 +0530

    uploading first version of news website

---
+++ index.js
@@ -0,0 +1,260 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+import 'dotenv/config';
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const apikey = process.env.API_KEY;
+
+app.get("/", async(req, res) => {
+ 
+  
+    try {
+      const response = await axios.get("https://newsapi.org/v2/top-headlines?country=in&pageSize=100" , {
+        headers: { 
+            'X-Api-Key':apikey
+        },
+      });
+    //   const result = JSON.stringify(response.data);
+      console.log(response.data);
+      console.log(response.data.totalResults);
+      let ind = response.data.totalResults;
+      console.log(ind);
+      res.render("index.ejs", { content : response.data , index : ind});
+    } catch (error) {
+      console.error("Failed to make request:", error.message);
+      res.render("index.ejs", {
+        error: error.message,
+      });
+    }
+  
+  
+  });
+
+
+  app.post("/search", async(req, res) => {
+    var content = req.body.find;
+    console.log(content);
+    try {
+      const response = await axios.get(`https://newsapi.org/v2/everything?q=${content}&pageSize=100` , {
+        headers: { 
+            'X-Api-Key':apikey
+        },
+      });    //   const result = JSON.stringify(response.data);
+      console.log(response.data);
+      console.log(response.data.totalResults);
+      let ind = response.data.totalResults;
+      console.log(ind);
+      res.render("index.ejs", { content : response.data , index : ind});
+    } catch (error) {
+      console.error("Failed to make request:", error.message);
+      res.render("index.ejs", {
+        error: error.message,
+      });
+    }
+  
+  
+  });
+
+  
+    app.get("/business", async(req, res) => {
+ 
+  
+      try {
+        const response = await axios.get("https://newsapi.org/v2/top-headlines?country=in&category=business&pageSize=100" , {
+          headers: { 
+              'X-Api-Key':apikey
+          },
+        });
+      //   const result = JSON.stringify(response.data);
+        console.log(response.data);
+        console.log(response.data.totalResults);
+        let ind = response.data.totalResults;
+        console.log(ind);
+        res.render("index.ejs", { content : response.data , index : ind});
+      } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+          error: error.message,
+        });
+      }
+    
+    
+    });
+
+
+    app.get("/entertainment", async(req, res) => {
+ 
+  
+      try {
+        const response = await axios.get("https://newsapi.org/v2/top-headlines?country=in&category=entertainment&pageSize=100" , {
+          headers: { 
+              'X-Api-Key':apikey
+          },
+        });
+      //   const result = JSON.stringify(response.data);
+        console.log(response.data);
+        console.log(response.data.totalResults);
+        let ind = response.data.totalResults;
+        console.log(ind);
+        res.render("index.ejs", { content : response.data , index : ind});
+      } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+          error: error.message,
+        });
+      }
+    
+    
+    });
+
+
+    app.get("/general", async(req, res) => {
+ 
+  
+      try {
+        const response = await axios.get("https://newsapi.org/v2/top-headlines?country=in&category=general&pageSize=100" , {
+          headers: { 
+              'X-Api-Key':apikey
+          },
+        });
+      //   const result = JSON.stringify(response.data);
+        console.log(response.data);
+        console.log(response.data.totalResults);
+        let ind = response.data.totalResults;
+        console.log(ind);
+        res.render("index.ejs", { content : response.data , index : ind});
+      } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+          error: error.message,
+        });
+      }
+    
+    
+    });
+
+
+    app.get("/health", async(req, res) => {
+ 
+  
+      try {
+        const response = await axios.get("https://newsapi.org/v2/top-headlines?country=in&category=health&pageSize=100" , {
+          headers: { 
+              'X-Api-Key':apikey
+          },
+        });
+      //   const result = JSON.stringify(response.data);
+        console.log(response.data);
+        console.log(response.data.totalResults);
+        let ind = response.data.totalResults;
+        console.log(ind);
+        res.render("index.ejs", { content : response.data , index : ind});
+      } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+          error: error.message,
+        });
+      }
+    
+    
+    });
+
+
+
+    app.get("/science", async(req, res) => {
+ 
+  
+      try {
+        const response = await axios.get("https://newsapi.org/v2/top-headlines?country=in&category=science&pageSize=100" , {
+          headers: { 
+              'X-Api-Key':apikey
+          },
+        });
+      //   const result = JSON.stringify(response.data);
+        console.log(response.data);
+        console.log(response.data.totalResults);
+        let ind = response.data.totalResults;
+        console.log(ind);
+        res.render("index.ejs", { content : response.data , index : ind});
+      } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+          error: error.message,
+        });
+      }
+    
+    
+    });
+
+
+    
+
+
+    app.get("/sports", async(req, res) => {
+ 
+  
+      try {
+        const response = await axios.get("https://newsapi.org/v2/top-headlines?country=in&category=sports&pageSize=100" , {
+          headers: { 
+              'X-Api-Key':apikey
+          },
+        });
+      //   const result = JSON.stringify(response.data);
+        console.log(response.data);
+        console.log(response.data.totalResults);
+        let ind = response.data.totalResults;
+        console.log(ind);
+        res.render("index.ejs", { content : response.data , index : ind});
+      } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+          error: error.message,
+        });
+      }
+    
+    
+    });
+
+    
+
+    
+
+
+    app.get("/technology", async(req, res) => {
+ 
+  
+      try {
+        const response = await axios.get("https://newsapi.org/v2/top-headlines?country=in&category=technology&pageSize=100" , {
+          headers: { 
+              'X-Api-Key':apikey
+          },
+        });
+      //   const result = JSON.stringify(response.data);
+        console.log(response.data);
+        console.log(response.data.totalResults);
+        let ind = response.data.totalResults;
+        console.log(ind);
+        res.render("index.ejs", { content : response.data , index : ind});
+      } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+          error: error.message,
+        });
+      }
+    
+    
+    });
+
+    
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+  });
+
